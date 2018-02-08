cb5a3a31b15e7dbdbbba2a7d806a86cc4872d2e3
Eli Dery <elidery@MacBook-Pro-3.local>
2018-02-07 22:29:00 -0700

    first real commit

---
+++ public/index.js
@@ -0,0 +1,7 @@
+console.log("does this work?");
+
+$("button").click(function() {
+  $("<h1>Downloading Virus...</h1>",
+  "<img src = https://giphy.com/gifs/progress-UUeAk8hdmDXX2>")
+  .replaceAll("#titleWrapper");
+})
